e3ef078bf72742c2213a4d015aeb65f1e275de98
Arthur Beck <averse.abfun@gmail.com>
2024-02-14 21:00:06 -0600

    Delete LethalVision/obj directory because why tf is this in here

--- LethalVision/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LethalCompanyTemplate
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LethalVision";
-        public const string PLUGIN_NAME = "LethalVision";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
